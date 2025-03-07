0d4495d5832a63e8b7df214345d207c6823e8bf9
Afey <gukalodenis@gmail.com>
2025-03-07 23:05:19 +0300

    #

---
+++ Дисциплины/АЯиП/АЯиП 2 семестр/Файлы/lab14_7.py
@@ -0,0 +1,21 @@
+def check(text):
+    brackets = []
+
+    for tp in text:
+        if tp == '(':
+            brackets.append(tp)
+        else:
+            if tp == ')':
+                if not brackets:
+                    return False
+                else:
+                    brackets.pop()
+
+    return len(brackets) == 0
+
+
+text = input("Введите текст для проверки: ")
+if check(text):
+    print("Скобки расставлены правильно.")
+else:
+    print("Скобки расставлены неправильно.")
